84d12cdf840b4130004ae6e4de94189c21cb86ae
Josh Long <josh@joshlong.com>
2021-04-19 14:20:56 -0700

    points

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 echo "going to write the points..."
-curl -i \
+curl  \
   -H "Accept: application/json" \
   -H "Content-Type:application/json" \
   -X POST --data '{"points": "2432"}'  http://localhost:8080/points/jlong
+
+echo
 
 echo "going to read the points..."
 curl http://localhost:8080/points/jlong
