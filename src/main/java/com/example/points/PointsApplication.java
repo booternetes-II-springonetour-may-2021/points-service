d05a63d74ad169a498c65ca6ca8d8025166165b8
Josh Long <josh@joshlong.com>
2021-04-21 10:06:01 -0700

    loosen payload

--- src/main/java/com/example/points/PointsApplication.java
+++ src/main/java/com/example/points/PointsApplication.java
@@ -51,7 +51,7 @@ class PointsRestController {
 
 	@PostMapping
 	Map<String, Object> write(@PathVariable String username, @RequestBody Map<String, Object> points) {
-		this.db.put(username, (Integer) points.get("points"));
+		this.db.put(username, Integer.parseInt((String) points.get("points")));
 		return this.pointsFor(username);
 	}
 }
