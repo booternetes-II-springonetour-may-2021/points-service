9d32749c0322d3fd66da84be8ca2d3a3226c3b4a
David Turanski <dturanski@vmware.com>
2021-04-21 14:18:02 -0400

    Expects points value to be integer now, e.g. {"points": 123}

--- src/main/java/com/example/points/PointsApplication.java
+++ src/main/java/com/example/points/PointsApplication.java
@@ -1,21 +1,18 @@
 package com.example.points;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
-
-import java.awt.*;
-import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PointsApplication {
@@ -51,10 +48,8 @@ class PointsRestController {
 
 	@PostMapping
 	Map<String, Object> write(@PathVariable String username, @RequestBody Map<String, Object> points) {
-		int add = Integer.parseInt((String) points.get("points"));
+		int add = (Integer) points.get("points");
 		int existing = this.db.getOrDefault(username, 0);
-//		System.out.println("adding: " + add);
-//		System.out.println("existing: " + existing);
 		this.db.put(username, existing + add);
 		return this.pointsFor(username);
 	}
