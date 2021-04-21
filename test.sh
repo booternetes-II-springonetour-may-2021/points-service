bed5f32121217c9211605441ed69d93113c20d81
Josh Long <josh@joshlong.com>
2021-04-21 10:41:08 -0700

    loosen payload

--- test.sh
+++ test.sh
@@ -4,13 +4,34 @@ def_host=localhost:8080
 my_host=${1:-${def_host}}
 
 echo  the host is "$my_host"
+
+echo "going to read the points, #1..."
+curl http://${my_host}/points/jlong
+
+
+
+
 echo "going to write the points..."
 curl \
   -H "Accept: application/json" \
   -H "Content-Type:application/json" \
-  -X POST --data '{"points": "2432"}' http://${my_host}/points/jlong
+  -X POST --data '{"points": "2"}' http://${my_host}/points/jlong
 
 echo
 
-echo "going to read the points..."
+echo "going to read the points again..."
+curl http://${my_host}/points/jlong
+
+
+
+
+echo "going to write the points..."
+curl \
+  -H "Accept: application/json" \
+  -H "Content-Type:application/json" \
+  -X POST --data '{"points": "4"}' http://${my_host}/points/jlong
+
+echo
+
+echo "going to read the points again..."
 curl http://${my_host}/points/jlong
