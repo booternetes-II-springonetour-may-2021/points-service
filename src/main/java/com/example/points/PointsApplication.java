9eacdac9b31327ced22752297f634c9cc581dc9f
David Turanski <dturanski@vmware.com>
2021-04-21 17:16:45 -0400

    Refactor logic into service class

--- src/main/java/com/example/points/PointsApplication.java
+++ src/main/java/com/example/points/PointsApplication.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,30 +28,47 @@ public class PointsApplication {
 @RestController
 class PointsRestController {
 
-	private final Map<String, Integer> db = new ConcurrentHashMap<>();
 
-	private Map<String, Object> pointsFor(String u) {
-		return pointsFor(u, this.db.get(u));
-	}
+	private final PointsService pointsService;
 
-	private Map<String, Object> pointsFor(String u, int i) {
-		return Map.of("username", u, "points", i);
+	PointsRestController(PointsService pointsService) {
+		this.pointsService = pointsService;
 	}
 
 	@GetMapping
 	ResponseEntity<Map<String, Object>> read(@PathVariable String username) {
-		if (this.db.containsKey(username)) {
-			return ResponseEntity.of(Optional.of(pointsFor(username)));
+		Map<String, Object> points = pointsService.pointsFor(username);
+		if (!points.isEmpty()) {
+			return ResponseEntity.of(Optional.of(points));
 		}
 
 		return ResponseEntity.notFound().build();
 	}
 
 	@PostMapping
-	Map<String, Object> write(@PathVariable String username, @RequestBody Map<String, Object> points) {
-		int add = (Integer) points.get("points");
-		int existing = this.db.getOrDefault(username, 0);
-		this.db.put(username, existing + add);
+	Map<String, Object> write(@PathVariable String username, @RequestBody Map<String, Integer> points) {
+		int add = points.get("points");
+		return pointsService.addPoints(username, add);
+	}
+}
+
+@Service
+class PointsService {
+	private final Map<String, Integer> db = new ConcurrentHashMap<>();
+
+	Map<String, Object> addPoints(String username, int points) {
+		int existing = (int) pointsFor(username).get("points");
+		this.db.put(username, existing + points);
 		return this.pointsFor(username);
 	}
+
+	Map<String, Object> pointsFor(String u) {
+		return pointsFor(u, this.db.getOrDefault(u, 0));
+	}
+
+	private Map<String, Object> pointsFor(String u, int i) {
+		return Map.of("username", u, "points", i);
+	}
+
+
 }
